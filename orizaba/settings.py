f9f9578c64f5e36b31e6391ecf767f0125e10d9f
Ben Taylor <bentaylor81@gmail.com>
2020-05-08 16:16:32 +0100

    Debug

--- orizaba/settings.py
+++ orizaba/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-#    'django.contrib.sessions.middleware.SessionMiddleware',
+#    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
                     os.path.join(BASE_DIR, 'static'),
                 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media Files # 
 
