ca0b3834e217d6607c44767fcc3eb19410c0bde4
Ben Taylor <bentaylor81@gmail.com>
2020-06-06 19:42:44 +0100

    API changes

--- app_apis/serializers.py
+++ app_apis/serializers.py
@@ -4,19 +4,19 @@ from app_websites.models import *
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Order
-        fields = ('order_id', 'order_no', 'billing_name', 'billing_address_1', 'billing_address_2', 'billing_city', 'billing_postcode', 'billing_country', 'billing_email', 'billing_phone', 'delivery_name', 'delivery_address_1', 'delivery_address_2', 'delivery_city', 'delivery_postcode', 'delivery_country', 'delivery_email', 'delivery_phone', 'delivery_price', 'ip_address', 'website', 'date', 'month', 'year' )
+        fields = '__all__'
 
 class OrderItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = OrderItem
-        fields = ('orderitem_id','order_id', 'product_id', 'item_price', 'item_qty', 'total_price')
+        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('product_id', 'product_name', 'sku', 'sell_price', 'buy_price', 'stock_qty', 'weight', 'location', 'brand', 'supplier', 'url')
+        fields = '__all__'
 
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Customer
-        fields = ('billing_email')
+        fields = '__all__'
