855c3eb869f90ea8abc0a6a7ff3188c79b9f4ad9
Ben Taylor <bentaylor81@gmail.com>
2020-07-05 20:19:12 +0100

    SendQTY

--- orizaba/settings.py
+++ orizaba/settings.py
@@ -72,7 +72,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'app_orders.context_processors.initial_status', # Loads initial Order Received Status
+                'app_orders.context_processors.initial_status', # Loads initial Order Received Status into OrderStatusHistory table
+                'app_orders.context_processors.initial_send_qty' # Add Send_Qty from the Item_Qty in OrderItems table
                 #'app_websites.context_processors.order_items', 
                 #'app_stats.context_processors.stats_sales_day', 
                 #'app_stats.context_processors.stats_sales_mon', 
