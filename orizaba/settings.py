61b54ec84026ad953b22fbe5f2f39e6b5a5ffaa9
Ben Taylor <bentaylor81@gmail.com>
2020-07-21 12:34:58 +0100

    Buildpack

--- orizaba/settings.py
+++ orizaba/settings.py
@@ -178,6 +178,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 # 3rd Party APIs
 
 # PrintNode
+
+
 PRINTNODE_URL = config('PRINTNODE_URL')
 PRINTNODE_AUTH = config('PRINTNODE_AUTH')
 PRINTNODE_LABEL_PRINTER = config('PRINTNODE_LABEL_PRINTER')
