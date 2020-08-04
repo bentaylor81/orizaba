09deb293511e87fc9441f3570646788095b5f08e
Ben Taylor <bentaylor81@gmail.com>
2020-08-04 17:13:53 +0100

    Label Formatting

--- app_websites/views.py
+++ app_websites/views.py
@@ -45,7 +45,6 @@ class ProductListView(FilterView):
             'copies' : '1',
            # 'page-width' : '100mm',
            # 'page-height' : '50mm',
-            'fit_to_page' : true, 
             'orientation' : 'Landscape',
             'margin-top': '0',
             'margin-right': '0',
@@ -71,7 +70,7 @@ class ProductListView(FilterView):
         printer = settings.PRINTNODE_LABEL_PRINTER
         content = "product-label.pdf" 
         copies = qty
-        payload = '{"printerId": ' +str(printer)+ ', "title": "Label for: ' +str(sku)+ ' ", "contentType": "pdf_uri", "content":"https://orizaba.herokuapp.com/static/pdf/' +str(content)+ '", "source": "GTS Product Label", "options": {"copies": ' +str(copies)+ '}}'
+        payload = '{"printerId": ' +str(printer)+ ', "title": "Label for: ' +str(sku)+ ' ", "contentType": "pdf_uri", "content":"https://orizaba.herokuapp.com/static/pdf/' +str(content)+ '", "source": "GTS Product Label", "options": { "fit_to_page" : "true", "copies": ' +str(copies)+ '}}'
         headers = {'Content-Type': 'application/json', 'Authorization': auth, }
 
         response = requests.request("POST", url, headers=headers, data=payload)
