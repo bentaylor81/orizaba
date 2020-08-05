256281290117a821c705b83061bdf7d0bd5c2778
Ben Taylor <bentaylor81@gmail.com>
2020-08-05 14:24:32 +0100

    Label Design

--- app_websites/views.py
+++ app_websites/views.py
@@ -74,8 +74,8 @@ class ProductListView(FilterView):
         payload = '{"printerId": ' +str(printer)+ ', "title": "Label for: ' +str(sku)+ ' ", "contentType": "pdf_uri", "content":"https://orizaba.herokuapp.com/static/pdf/' +str(content)+ '", "source": "GTS Product Label", "options": {"copies": ' +str(copies)+ '}}'
         headers = {'Content-Type': 'application/json', 'Authorization': auth, }
 
-        #response = requests.request("POST", url, headers=headers, data=payload)
-        #print(response.text.encode('utf8'))
+        response = requests.request("POST", url, headers=headers, data=payload)
+        print(response.text.encode('utf8'))
 
         return HttpResponseRedirect(path)
 
