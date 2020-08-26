caeb96c930e3aa9e708acd99863f882fa4783fbb
Ben Taylor <bentaylor81@gmail.com>
2020-08-26 13:22:31 +0100

    Product table ordering

--- app_products/filters.py
+++ app_products/filters.py
@@ -1,17 +1,33 @@
-import django_filters
+from django_filters import rest_framework as filters
 from .models import *
 
-class ProductFilter(django_filters.FilterSet):
+class ProductFilter(filters.FilterSet):
 
-    Product = django_filters.CharFilter(field_name='product_name', label='Product', lookup_expr='icontains')
-    Sku = django_filters.CharFilter(field_name='sku', label='SKU', lookup_expr='icontains')
+    Product = filters.CharFilter(field_name='product_name', label='Product', lookup_expr='icontains')
+    Sku = filters.CharFilter(field_name='sku', label='SKU', lookup_expr='icontains')
+
+    o = filters.OrderingFilter(
+
+        choices=(
+            ('location', 'Location'),
+            ('-location', 'Location (desc)'),
+            ('sell_price', 'Sell Price'),
+            ('-sell_price', 'Sell Price (desc)'),
+            ('sku', 'SKU'),
+            ('-sku', 'SKU (desc)'),
+            ('stock_qty', 'Stock Qty'),
+            ('-stock_qty', 'Stock Qty (desc)'),
+            ('weight', 'Weight'),
+            ('-weight', 'Weight (desc)'),
+        ),
+    )
 
     class Meta:
         model = Product
         fields = ['Product', 'Sku', 'brand', 'supplier']
         
 
-class SupplierProductFilter(django_filters.FilterSet):
+class SupplierProductFilter(filters.FilterSet):
     
     class Meta:
         model = Product
