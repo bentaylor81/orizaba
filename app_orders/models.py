4aa2ff4b5ba8e6b8eccd81c58cce46cff466c7c9
Ben Taylor <bentaylor81@gmail.com>
2020-10-15 21:04:23 +0100

    Stock control

--- app_orders/models.py
+++ app_orders/models.py
@@ -62,7 +62,7 @@ class OrderItem(models.Model):
         return str(self.order_id) + ' | ' + str(self.item_qty)
 
     class Meta:
-        ordering = ["-order_id", "-send_qty"]
+        ordering = ["-order_id__date", "-send_qty"]
 
 class Order(models.Model):
     order_id = models.IntegerField(primary_key=True)
