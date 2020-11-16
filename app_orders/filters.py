8b8f83e3ffdf25d2a6dff421bbce67d268a95a44
Ben Taylor <bentaylor81@gmail.com>
2020-11-16 17:56:36 +0000

    ShipTheory Token

--- app_orders/filters.py
+++ app_orders/filters.py
@@ -4,12 +4,12 @@ from .models import *
 
 class OrderFilter(filters.FilterSet):
 
-    Order = filters.CharFilter(field_name='order_no', label='Order Number', lookup_expr='icontains')
-    BillingName = filters.CharFilter(field_name='billing_name', label='Billing Name', lookup_expr='icontains')       
+    order = filters.CharFilter(field_name='order_no', label='Order Number', lookup_expr='icontains')
+    billing_last_name = filters.CharFilter(field_name='billing_lastname', label='Billing Surname', lookup_expr='icontains')       
 #    Date = filters.DateFromToRangeFilter(field_name='date', label=['Date From', 'to'])
     start_date = filters.DateFilter(field_name='date', lookup_expr=('gt'), label='Date From', widget=forms.DateInput(attrs={'type': 'date', 'class': 'date-input'}))
     end_date = filters.DateFilter(field_name='date', lookup_expr=('lt'), label='to', widget=forms.DateInput(attrs={'type': 'date', 'class': 'date-input'}))
 
     class Meta:
         model = Order
-        fields = ['Order', 'BillingName', 'status_current', 'start_date', 'end_date']
+        fields = ['Order', 'billing_lastname', 'status_current', 'start_date', 'end_date']
