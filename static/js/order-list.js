50ebd330e99bd94c095781f1733f63456b69f5b0
Ben Taylor <bentaylor81@gmail.com>
2020-11-25 05:35:29 +0000

    Order Status

--- static/js/order-list.js
+++ static/js/order-list.js
@@ -4,7 +4,6 @@ var queryAll = document.querySelectorAll.bind(document);
 // SET THE BACKGROUND COLOUR OF THE ROW BASED ON ORDER STATUS
 var tableRow = queryAll('#order-row');
 var orderStatus = document.querySelectorAll('.status span');
-    console.log(orderStatus)
 
     for(let i=0; i < tableRow.length; i++){
         console.log(orderStatus[i].innerHTML)
